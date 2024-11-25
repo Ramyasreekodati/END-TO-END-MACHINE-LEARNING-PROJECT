614ab7cf1de211b5694eb7a624e691aff4b33e65
Ramyasreekodati <ramyasreekodati@gmail.com>
2024-11-25 10:15:18 +0530

    initial commit

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 from src.wine_quality import logger
 from src.wine_quality.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.wine_quality.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
-
+from src.wine_quality.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 
 
 STAGE_NAME="Data Ingestion stage"
@@ -27,6 +27,20 @@ if __name__ == "__main__":
         obj = DataValidationTrainingPipeline()
         obj.main()
         logger.info(f" >>>>>> stage {STAGE_NAME} completed<<<<<<\n\n X===================X")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+    
+
+
+STAGE_NAME = "Data Transformation stage"
+
+if __name__ == "__main__":
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+        data_ingestion = DataTransformationTrainingPipeline()
+        data_ingestion.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     except Exception as e:
         logger.exception(e)
         raise e
