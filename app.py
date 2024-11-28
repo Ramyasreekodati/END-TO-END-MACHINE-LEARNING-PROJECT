fea595da2331c95bc98ac894addd22d5dc022eac
Ramyasreekodati <ramyasreekodati@gmail.com>
2024-11-28 12:26:13 +0530

    initial commit

--- app.py
+++ app.py
@@ -106,4 +106,4 @@ def logout():
     return redirect(url_for("signin"))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=2003, debug=True)
+    app.run(host="0.0.0.0", port=2021, debug=True)
